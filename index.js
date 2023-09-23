0dd7c064cbea9cafd5d24c8545f5afcf01b1a585
Fellipe Negherbon Prim <99996732+FellipePrim@users.noreply.github.com>
2023-09-23 11:28:28 -0300

    Aprimoramento JS

---
+++ index.js
@@ -0,0 +1,18 @@
+//Identifica as mudanças feitas
+
+function update(){
+    let bill = Number(document.getElementById('yourBill').value)
+    let tipPercent = document.getElementById('tipInput').value
+    let split = document.getElementById('splitInput').value
+    // confere se os valores dos inputs são armazendados e mostrados no console
+    //console.log(bill, tipPercent, split)
+
+    let tipValue = bill * (tipPercent / 100)
+    //console.log(tipPercent)
+    let billTotal = bill + tipValue
+    let billEach = billTotal / split
+    //console.log(billEach)
+
+    document.getElementById('tipPercent').innerHTML = tipPercent + ' %'
+}
+
